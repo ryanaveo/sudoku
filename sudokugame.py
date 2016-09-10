7df7f9cd4927c14048704d1cbdb26d0b911da32c
RyanAveo <ryan.aveo@gmail.com>
2016-09-10 10:52:36 -0700

    Add tests for validate_move, undo_move, redo_move

--- sudokugame.py
+++ sudokugame.py
@@ -8,183 +8,185 @@ from collections import namedtuple
 Move = namedtuple('Move', 'move_type row col num')
 
 class Game():
-	'''Handles all the game logic and interfaces with the board to execute moves.'''
+    '''Handles all the game logic and interfaces with the board to execute moves.'''
 
-	def __init__(self):
-		'Starts a new game'
-		self._board = board.Board()
-		self._undo_list = []
-		self._redo_list = []
-			
-	def new_game(self):
-		'Creates a new board to start a new game'
-		pass
+    def __init__(self):
+        'Starts a new game'
+        self._board = board.Board()
+        self._undo_list = []
+        self._redo_list = []
+            
+    def new_game(self):
+        'Creates a new board to start a new game'
+        pass
 
-	def get_board(self):
-		'Returns a representation of the game board.'
-		pass
+    def get_board(self):
+        'Returns a representation of the game board.'
+        pass
 
-	def open_cells(self):
-		'Returns a list of the cells that are empty.'
-		pass
+    def open_cells(self):
+        'Returns a list of the cells that are empty.'
+        pass
 
-	def add_number(self, row: int, col: int, number: int):
-		"Executes a move if it's valid. Adds move to undo list and clears redo list"
-		self._validate_move(row, col, number)
-		self._board.add(row, col, number)
+    def add_number(self, row: int, col: int, number: int):
+        "Executes a move if it's valid. Adds move to undo list and clears redo list"
+        self._validate_move(row, col, number)
+        self._board.add(row, col, number)
 
-		move = self._store_move('add', row, col, number)
-		self._undo_list.append(move)
-		self._redo_list = []
-
-
-	def undo_move(self):
-		'Undoes the last move if there are any. Otherwise raises exception.'
-		if len(self._undo_list) == 0:
-			raise UndoError
-
-		else:
-			last_move = self._undo_list.pop()
-			if last_move.move_type == 'add':
-				self._board.clear(last_move.row, last_move.col)
-			elif last_move.move_type == 'remove':
-				self._board.add(last_move.row, last_move.col, last_move.number)
-			
-			self._redo_list.append(last_move)
+        move = self._store_move('add', row, col, number)
+        self._undo_list.append(move)
+        self._redo_list = []
 
 
-	def redo_move(self):
-		'Redoes the last undo if there are any. Otherwise raises exception.'
-		if len(self._redo_list) == 0:
-			raise RedoError
+    def undo_move(self):
+        'Undoes the last move if there are any. Otherwise raises exception.'
+        if len(self._undo_list) == 0:
+            raise UndoError
 
-		else:
-			last_undo = self._redo_list.pop()
-			if last_undo.move_type == 'add':
-				self._board.add(last_undo.row, last_undo.col, last_undo.number)
-			elif last_undo.move_type == 'remove':
-				self._board.clear(last_undo.row, last_undo.col)
-			
-			self._undo_list.append(last_undo)
+        else:
+            last_move = self._undo_list.pop()
+            if last_move.move_type == 'add':
+                self._board.clear(last_move.row, last_move.col)
+            elif last_move.move_type == 'remove':
+                self._board.add(last_move.row, last_move.col, last_move.num)
+            
+            self._redo_list.append(last_move)
 
 
-	def remove_number(self, row: int, col: int):
-		"Remove a number from the given cell"
-		number = self._board.clear(row, col)
-		move = self._store_move('remove', row, col, number)
-		
-		self._undo_list.append(move)
-		self._redo_list = []
+    def redo_move(self):
+        'Redoes the last undo if there are any. Otherwise raises exception.'
+        if len(self._redo_list) == 0:
+            raise RedoError
 
-	def save_state(self, state_name: str='save'):
-		'''
-		Saves the current state. Uses state_name to name the csv file in
-		the board's to_csv call. 
-		'''
-		pass
+        else:
+            last_undo = self._redo_list.pop()
+            if last_undo.move_type == 'add':
+                self._board.add(last_undo.row, last_undo.col, last_undo.num)
+            elif last_undo.move_type == 'remove':
+                self._board.clear(last_undo.row, last_undo.col)
+            
+            self._undo_list.append(last_undo)
 
-	def load_state(self, state_name: str='save'):
-		'Loads a board state'
-		pass
 
-	def check_victory(self) -> bool:
-		'Checks the victory conditions have been met.'
-		pass
+    def remove_number(self, row: int, col: int):
+        "Remove a number from the given cell"
+        number = self._board.clear(row, col)
+        move = self._store_move('remove', row, col, number)
+        
+        self._undo_list.append(move)
+        self._redo_list = []
 
-	def print_board(self):
-		"Prints the board using the board's print_board method"
-		pass
+    def save_state(self, state_name: str='save'):
+        '''
+        Saves the current state. Uses state_name to name the csv file in
+        the board's to_csv call. 
+        '''
+        pass
 
-	def _store_move(self, move_type: str, row: int, col: int, number: int):
-		'Creates a Move namedtuple that stores information about the move for undo and redo. Type will be "add" or "remove" depending on what was done'
-		move = Move(move_type, row, col, number)
-		
-		return move
+    def load_state(self, state_name: str='save'):
+        'Loads a board state'
+        pass
 
-	def _validate_move(self, row: int, col: int, number: int):
-		'''
-		Raises an exception if the given move is valid on the board.
-		Needs to check:
-			(1) Cell is within the board's bounds
-			(2) Cell is not occupied
-			(3) The move doesn't have any of the same entry in the same column, row, or box.
-			(4) The number is valid (1-9)
-		'''
-		if self._cell_is_occupied(self, row: int, col: int):
-			raise OccupiedCellError
+    def check_victory(self) -> bool:
+        'Checks the victory conditions have been met.'
+        pass
 
-		if not self._cell_in_bounds(self, row: int, col: int):
-			raise CellOutOfBoundsError
+    def print_board(self):
+        "Prints the board using the board's print_board method"
+        pass
 
-		if self._same_num_in_row(self, row: int, number: int):
-			raise SameRowError
+    def _store_move(self, move_type: str, row: int, col: int, number: int):
+        'Creates a Move namedtuple that stores information about the move for undo and redo. Type will be "add" or "remove" depending on what was done'
+        move = Move(move_type, row, col, number)
+        
+        return move
 
-		if self._same_num_in_col(self, col: int, number: int):
-			raise SameColumnError
-		
-		if self._same_num_in_box(self, row: int, col: int, number: int):
-			raise SameBoxError
+    def _validate_move(self, row: int, col: int, number: int):
+        '''
+        Raises an exception if the given move is valid on the board.
+        Needs to check:
+            (1) Cell is within the board's bounds
+            (2) Cell is not occupied
+            (3) The move doesn't have any of the same entry in the same column, row, or box.
+            (4) The number is valid (1-9)
+        '''
+        if number == 0:
+                        raise InvalidNumberError
+                
+        elif self._cell_is_occupied(row, col):
+            raise OccupiedCellError
 
-		if not self._valid_number(self, number: int):
-			raise InvalidNumberError
+        elif not self._cell_in_bounds(row, col):
+            raise CellOutOfBoundsError
 
-	def _cell_is_occupied(self, row: int, col: int):
-		'Returns a bool that indicates if a cell is occupied'
-		return self._board._state[row][col] != 0
+        elif self._same_num_in_row(row, number):
+            raise SameRowError
 
-	def _cell_in_bounds(self, row: int, col: int):
-		'Returns a bool that indicates if the given row and column are in the dimension of the board'
+        elif self._same_num_in_col(col, number):
+            raise SameColumnError
+        
+        elif self._same_num_in_box(row, col, number):
+            raise SameBoxError
 
-		return (0 <= row <= 8) and (0 <= col <= 8)
+        elif not self._valid_number(number):
+            raise InvalidNumberError
 
-	def _same_num_in_row(self, row: int, number: int):
-		'Returns a bool that indicates if the given number is already in the given row'
+    def _cell_is_occupied(self, row: int, col: int):
+        'Returns a bool that indicates if a cell is occupied'
+        return self._board._state[row][col] != 0
 
-		return number in self._board.get_row(row)
+    def _cell_in_bounds(self, row: int, col: int):
+        'Returns a bool that indicates if the given row and column are in the dimension of the board'
 
-	def _same_num_in_col(self, col: int, number: int):
-		'Returns a bool that indicates if the given number is already in the given column'
+        return (0 <= row <= 8) and (0 <= col <= 8)
 
-		return number in self._board.get_column(col)
+    def _same_num_in_row(self, row: int, number: int):
+        'Returns a bool that indicates if the given number is already in the given row'
+        return number in self._board.get_row(row)
 
-	def _same_num_in_box(self, row: int, col: int, number: int):
-		'Returns a bool that indicates if the given number is already in the respective box of the given cell'
+    def _same_num_in_col(self, col: int, number: int):
+        'Returns a bool that indicates if the given number is already in the given column'
 
-		return number in self._board.get_box(row, col)
+        return number in self._board.get_column(col)
 
-	def _valid_number(self, number: int):
-		'Returns a bool that indicates if the given number is valid'
+    def _same_num_in_box(self, row: int, col: int, number: int):
+        'Returns a bool that indicates if the given number is already in the respective box of the given cell'
 
-		return 1 <= number <= 9
+        return number in self._board.get_box(row, col)
+
+    def _valid_number(self, number: int):
+        'Returns a bool that indicates if the given number is valid'
+
+        return 1 <= number <= 9
 
 class OccupiedCellError(Exception):
-	'Exception for trying to make a move on an occupied cell.'
-	pass
+    'Exception for trying to make a move on an occupied cell.'
+    pass
 
 class CellOutOfBoundsError(Exception):
-	'Exception for giving coordinates that are outside of the board.'
-	pass
+    'Exception for giving coordinates that are outside of the board.'
+    pass
 
 class SameRowError(Exception):
-	'Exception for moves whose numbers have already been seen in the same row.'
-	pass
+    'Exception for moves whose numbers have already been seen in the same row.'
+    pass
 
 class SameColumnError(Exception):
-	'Exception for moves whose numbers have already been seen in the same column.'
-	pass
+    'Exception for moves whose numbers have already been seen in the same column.'
+    pass
 
 class SameBoxError(Exception):
-	'Exception for moves whose numbers have already been seen in the same box.'
-	pass
+    'Exception for moves whose numbers have already been seen in the same box.'
+    pass
 
 class InvalidNumberError(Exception):
-	'Exception for attempts to place an invalid number (any number not 1-9) in a cell'
-	pass
+    'Exception for attempts to place an invalid number (any number not 1-9) in a cell'
+    pass
 
 class UndoError(Exception):
-	'Exception for attempts to undo when there are no moves to undo'
-	pass
+    'Exception for attempts to undo when there are no moves to undo'
+    pass
 
 class RedoError(Exception):
-	'Exception for attempts to redo when there are no moves to redo'
-	pass
+    'Exception for attempts to redo when there are no moves to redo'
+    pass
--- sudokugame_unittest.py
+++ sudokugame_unittest.py
@@ -4,81 +4,118 @@ import sudokugame
 import board
 import unittest
 
-class BoardTestCase(unittest.TestCase):
-	'Tests for "sudokugame.py"'
+class GameTestCase(unittest.TestCase):
+    'Tests for "sudokugame.py"'
 
-	def setUp(self):
-		self._test_board = board.Board()
-		self._game = sudokugame.self._game()
-		
-		win_board = [[5,3,4,6,7,8,9,1,2],
-		[6,7,2,1,9,5,3,4,8],
-		[1,9,8,3,4,2,5,6,7],
-		[8,5,9,7,6,1,4,2,3],
-		[4,2,6,8,5,3,7,9,1],
-		[7,1,3,9,2,4,8,5,6],
-		[9,6,1,5,3,7,2,8,4],
-		[2,8,7,4,1,9,6,3,5],
-		[3,4,5,2,8,6,1,7,9]]
+    def setUp(self):
+        self._game = sudokugame.Game()
+        
+        self._win_board = [[5,3,4,6,7,8,9,1,2],
+        [6,7,2,1,9,5,3,4,8],
+        [1,9,8,3,4,2,5,6,7],
+        [8,5,9,7,6,1,4,2,3],
+        [4,2,6,8,5,3,7,9,1],
+        [7,1,3,9,2,4,8,5,6],
+        [9,6,1,5,3,7,2,8,4],
+        [2,8,7,4,1,9,6,3,5],
+        [3,4,5,2,8,6,1,7,9]]
 
-	def test_valid_move(self):
-		self.assertTrue(self._game.valid_move(0,0,1))
-		self.assertFalse(self._game.valid_move(9,9,1))
+    def test_open_cells(self):
+        self.assertEqual(len(self._game.open_cells()), 81)
 
-		self._test_board.add(0,0,2)
+        self._game._board.set_board([
+            [1,2,3,4,5,6,7,8,9],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]])
 
-		self.assertFalse(self._game.valid_move(0,0,1))
+        self.assertEqual(len(self._game.open_cells), 72)
+        self.assertEqual(self._game.open_cells, 
+            [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),
+            (2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(2,8),
+            (3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7),(3,8),
+            (4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(4,8),
+            (5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),
+            (6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),
+            (7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7),(7,8),
+            (8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8)])
 
-		self.assertFalse(self._game.valid_move(0,3,2))
-		self.assertFalse(self._game.valid_move(5,0,2))
-		self.assertFalse(self._game.valid_move(2,2,2))
+    def test_add_number(self):
+        self._game.add_number(1,1,3)
 
-	def test_open_cells(self):
-		self.assertEqual(len(self._game.open_cells), 81)
+        self.assertEqual(self._game._board.get_cell(1,1), 3)
 
-		self._test_board.set_board([
-			[1,2,3,4,5,6,7,8,9],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0]])
+        self.assertEqual(self._game._undo_list, [(sudokugame.Move(move_type = 'add', row = 1, col = 1, num = 3))])
+        self.assertEqual(len(self._game._redo_list), 0)
 
-		self.assertEqual(len(self._game.open_cells), 72)
-		self.assertEqual(self._game.open_cells, 
-			[(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),
-			(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(2,8),
-			(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7),(3,8),
-			(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(4,8),
-			(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),
-			(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),
-			(7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7),(7,8),
-			(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8)])
+    def test_undo_move(self):
+    	self.assertRaises(sudokugame.UndoError, self._game.undo_move)
 
-	def test_make_move(self):
-		self._test_board.add(1,1,3)
+    	self._game.add_number(1,1,3)
+    	self._game.undo_move()
 
-		self.assertRaises(OccupiedCellException, self._game.make_move(), 1, 1, 1)
-		self.assertRaises(CellOutOfBoundsException, self._game.make_move(), 9, 9, 1)
-		self.assertRaises(SameRowException, self._game.make_move(), 1, 2, 3)
-		self.assertRaises(SameColumnException, self._game.make_move(), 5, 1, 3)
-		self.assertRaises(SameBoxException, self._game.make_move(), 0, 0, 3)
+    	self.assertEqual(self._game._board._state[1][1], 0)
+    	self.assertEqual(self._game._redo_list, [sudokugame.Move(move_type = 'add', row = 1, col = 1, num = 3)])
 
-	def remove(self):
-		self._game.make_move(1,1,9)
-		self.assertEqual(self._test_board.get_cell(1,1), 9)
+    	self._game.add_number(5,5,4)
+    	self._game.remove_number(5,5)
+    	self._game.undo_move()
 
-		self._game.remove(1,1)
-		self.assertFalse(self._test_board.get_cell(1,1) == 9)
-		self.assertEqual(self._test_board.get_cell(1,1), 0)
+    	self.assertEqual(self._game._board._state[5][5], 4)
+    	self.assertEqual(self._game._redo_list, [sudokugame.Move(move_type = 'remove', row = 5, col = 5, num = 4)])
 
-	def check_victory(self):
-		self.assertFalse(self._game.check_victory())
+    def test_redo_move(self):
+    	self.assertRaises(sudokugame.RedoError, self._game.redo_move)
 
-		self._test_board.set_board(win_board)
+    	self._game.add_number(2,2,3)
+    	self._game.undo_move()
+    	self._game.redo_move()
 
-		self.assertTrue(self._game.check_victory())
+    	self.assertEqual(self._game._board._state[2][2], 3)
+    	self.assertEqual(self._game._undo_list, [sudokugame.Move(move_type = 'add', row = 2, col = 2, num = 3)])
 
+    	self._game.add_number(8,8,5)
+    	self._game.remove_number(8,8)
+    	self._game.undo_move()
+    	self._game.redo_move()
+
+    	self.assertEqual(self._game._board._state[8][8], 0)
+    	self.assertEqual(self._game._undo_list, [sudokugame.Move(move_type='add', row=2, col=2, num=3), sudokugame.Move(move_type='add', row=8, col=8, num=5), sudokugame.Move(move_type='remove', row=8, col=8, num=5)])
+
+    def test_remove_number(self):
+        self._game.add_number(1,1,9)
+        self._game.remove_number(1,1)
+        
+        self.assertFalse(self._game._board.get_cell(1,1) == 9)
+        self.assertEqual(self._game._board.get_cell(1,1), 0)
+
+        self.assertEqual(self._game._undo_list, [sudokugame.Move(move_type = 'add', row = 1, col = 1, num = 9), sudokugame.Move(move_type = 'remove', row = 1, col = 1, num = 9)])
+
+        self.assertEqual(len(self._game._redo_list), 0)
+
+    def test_check_victory(self):
+        self.assertFalse(self._game.check_victory())
+
+        self._game._board.set_board(self._win_board)
+
+        self.assertTrue(self._game.check_victory())
+
+    def test_validate_move(self):
+        self._game._board.add(0,1,2)
+
+        self.assertRaises(sudokugame.OccupiedCellError, self._game._validate_move, 0, 1, 3)
+        self.assertRaises((sudokugame.CellOutOfBoundsError, IndexError), self._game._validate_move, 9, 9, 1)
+        self.assertRaises(sudokugame.SameRowError, self._game._validate_move, 0, 2, 2)
+        self.assertRaises(sudokugame.SameColumnError, self._game._validate_move, 5, 1, 2)
+        self.assertRaises(sudokugame.SameBoxError, self._game._validate_move, 2, 2, 2)
+
+        self.assertRaises(sudokugame.InvalidNumberError, self._game._validate_move, 4, 4, -5)
+        self.assertRaises(sudokugame.InvalidNumberError, self._game._validate_move, 4, 4, 0)
+        self.assertRaises(sudokugame.InvalidNumberError, self._game._validate_move, 4, 4, 10)
+if __name__ == '__main__':
+    unittest.main()
